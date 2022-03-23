c7c95e75940690c658d211ce5cfaea4a70d16200
MarKoz095 <slimshady0955@gmail.com>
2022-03-23 21:45:45 +0100

    Adds Calculator Spring-MVC

---
+++ Spring/Calculator-Spring-MVC/src/test/java/pl/marcin/kalkulator/KalkulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcin.kalkulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalkulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
